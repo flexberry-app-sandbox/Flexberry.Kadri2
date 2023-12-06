bfe78c9e7e55a1a6d86b23910674e8f1707acfd6
Flexberry-man <mail@flexberry.net>
2023-12-06 15:53:52 +0000

    Flexberry code generation

--- src/ember-app/app/controllers/index.js
+++ src/ember-app/app/controllers/index.js
@@ -22,7 +22,7 @@ export default Controller.extend({
             link: 'i-i-s-kadri-отделы-l',
             caption: i18n.t('forms.application.sitemap.отдел-кадров.i-i-s-kadri-отделы-l.caption'),
             title: i18n.t('forms.application.sitemap.отдел-кадров.i-i-s-kadri-отделы-l.title'),
-            icon: 'phone',
+            icon: 'tasks',
             children: null
           }, {
             link: 'i-i-s-kadri-контроль-т-з-l',
@@ -34,19 +34,19 @@ export default Controller.extend({
             link: 'i-i-s-kadri-должности-l',
             caption: i18n.t('forms.application.sitemap.отдел-кадров.i-i-s-kadri-должности-l.caption'),
             title: i18n.t('forms.application.sitemap.отдел-кадров.i-i-s-kadri-должности-l.title'),
-            icon: 'tasks',
+            icon: 'book',
             children: null
           }, {
             link: 'i-i-s-kadri-анкеты-l',
             caption: i18n.t('forms.application.sitemap.отдел-кадров.i-i-s-kadri-анкеты-l.caption'),
             title: i18n.t('forms.application.sitemap.отдел-кадров.i-i-s-kadri-анкеты-l.title'),
-            icon: 'folder',
+            icon: 'briefcase',
             children: null
           }, {
             link: 'i-i-s-kadri-сотрудники-l',
             caption: i18n.t('forms.application.sitemap.отдел-кадров.i-i-s-kadri-сотрудники-l.caption'),
             title: i18n.t('forms.application.sitemap.отдел-кадров.i-i-s-kadri-сотрудники-l.title'),
-            icon: 'chart line',
+            icon: 'list',
             children: null
           }]
         }
